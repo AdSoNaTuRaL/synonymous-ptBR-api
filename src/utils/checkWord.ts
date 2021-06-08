1d79af7804e42a0c085c123eb0f8c75db05afb5e
adsonatural <adsonhenriquesilva@gmail.com>
2021-06-08 15:02:24 +0200

    format code with prettier

--- src/utils/checkWord.ts
+++ src/utils/checkWord.ts
@@ -1,8 +1,7 @@
 export function isWord(word: string): boolean {
-    if (/\s/g.test(word)) return false;
-    if (/[-!$%^&*()_+|~=`{}\[\]:";'<>?,.\/]/.test(word)) return false;
-    if (/[0-9]/.test(word)) return false;
-  
-    return true;
-  }
-  
+  if (/\s/g.test(word)) return false;
+  if (/[-!$%^&*()_+|~=`{}\[\]:";'<>?,.\/]/.test(word)) return false;
+  if (/[0-9]/.test(word)) return false;
+
+  return true;
+}
