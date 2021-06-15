7740df20342b82918209456b8ec9787e10b8a6d0
adsonatural <adsonhenriquesilva@gmail.com>
2021-06-15 16:22:48 +0200

    Refactor server

--- src/server.ts
+++ src/server.ts
@@ -1,37 +1,12 @@
 import express from 'express';
 import cors from 'cors';
-import { isWord } from './utils/checkWord';
-import { getPortugueseSynonymous } from './services/getPortugueseSynonymous';
+import router from './routes';
 
 const app = express();
+
 app.use(express.json());
 app.use(cors());
-
-app.get('/', (_, res) => {
-  return res.json({ message: 'Hello World' });
-});
-
-app.get('/search', (req, res) => {
-  const { q } = req.query;
-
-  if (q) {
-    const isRealWord = isWord(q as string);
-
-    if (!isRealWord) {
-      return res.status(400).json({ error: 'Invalid word' });
-    }
-
-    getPortugueseSynonymous(q as string, (error, errorMessage, result) => {
-      if (error) {
-        return res.status(500).json({ error, errorMessage });
-      }
-
-      return res.json({ synonyms: result });
-    });
-  } else {
-    return res.status(400).json({ error: 'Invalid parameters' });
-  }
-});
+app.use(router);
 
 app.listen(3333, () => {
   console.log('🚀 Server runing');
