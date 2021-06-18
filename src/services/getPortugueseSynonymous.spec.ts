4156f214e8592793d605d4b0d8e4f75ef705c1f0
adsonatural <adsonhenriquesilva@gmail.com>
2021-06-18 13:27:36 +0200

    add new test case

--- src/services/getPortugueseSynonymous.spec.ts
+++ src/services/getPortugueseSynonymous.spec.ts
@@ -1,3 +1,4 @@
+import AppError from '../errors/AppError';
 import { getPortugueseSynonymous } from './getPortugueseSynonymous';
 
 describe('Get a list of synonyms, given a word', () => {
@@ -5,5 +6,11 @@ describe('Get a list of synonyms, given a word', () => {
     const result = await getPortugueseSynonymous('sacada');
 
     expect(Array.isArray(result)).toBe(true);
+  });
+
+  it('should check if given a invalid word, throw a error', async () => {
+    await expect(getPortugueseSynonymous('aa')).rejects.toEqual(
+      new AppError('Internal Server Error'),
+    );
   });
 });
