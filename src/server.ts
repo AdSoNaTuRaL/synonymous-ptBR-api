b8ed65758f85125573a5bdf1c7f1226b11597c81
adsonatural <adsonhenriquesilva@gmail.com>
2021-06-28 11:26:36 +0200

    config port (heroku deploy)

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import app from './app';
 
-app.listen(3333, () => {
-  console.log('🚀 Server runing on port 3333');
+const PORT = process.env.PORT || 3333;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server runing on port ${PORT}`);
 });
